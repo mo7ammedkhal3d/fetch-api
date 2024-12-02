f69655416d340255c9f01bc5c6872b56a0b60778
Mo'Khaled Bin-Hazem <Mo7ammedkhal3d@gmail.com>
2024-12-02 19:29:32 +0300

    using async & await nested fetch chining

--- src/App.js
+++ src/App.js
@@ -22,21 +22,18 @@ function App() {
   //   },
   // ];
 
-  const fetchMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films/')
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+  const fetchMoviesHandler = async () => {
+    const response = await fetch('https://swapi.dev/api/films/')
+    const data = await response.json();
+     
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    setMovies(transformedMovies);
     });
     
   };
